4e01a4a0d481607c3069311e4871a9b21c69766f
Jomo Kigo <kigojay684@gmail.com>
2023-10-20 16:46:39 +0300

    modified:   src/script.js 	modified:   src/styles.css 	modified:   src/styles.css.map 	modified:   src/styles.scss

--- src/script.js
+++ src/script.js
@@ -11,90 +11,145 @@ const closeMenuBtn = document.getElementById("close-menu");
 const menuLinks = document.querySelectorAll(".ul-link");
 
 // Define a function to execute when the display size is above a certain width
+// function executeOnLargeScreen() {
+//   // Your code for larger screens here
+
+//   introLink.addEventListener("click", () => {
+//     links.style.transform = "translateX(calc(50% - 75px))";
+//     introLink.classList.add("active");
+//     myWorkLinks.forEach((link) => {
+//       link.classList.remove("active");
+//     });
+//     learnMoreLinks.forEach((link) => {
+//       link.classList.remove("active");
+//     });
+//     contactLink.classList.remove("active");
+//   });
+
+//   myWorkLinks.forEach((link) => {
+//     link.addEventListener("click", () => {
+//       links.style.transform = "translateX(calc(25% - 75px))";
+//       introLink.classList.remove("active");
+//       myWorkLinks.forEach((link) => {
+//         link.classList.add("active");
+//       });
+//       learnMoreLinks.forEach((link) => {
+//         link.classList.remove("active");
+//       });
+//       contactLink.classList.remove("active");
+//     });
+//   });
+
+//   learnMoreLinks.forEach((link) => {
+//     link.addEventListener("click", () => {
+//       links.style.transform = "translateX(calc(-25% + 75px))";
+//       introLink.classList.remove("active");
+//       myWorkLinks.forEach((link) => {
+//         link.classList.remove("active");
+//       });
+//       learnMoreLinks.forEach((link) => {
+//         link.classList.add("active");
+//       });
+//       contactLink.classList.remove("active");
+//     });
+//   });
+//   contactLink.addEventListener("click", () => {
+//     links.style.transform = "translateX(calc(-50% + 75px))";
+//     introLink.classList.remove("active");
+//     myWorkLinks.forEach((link) => {
+//       link.classList.remove("active");
+//     });
+//     learnMoreLinks.forEach((link) => {
+//       link.classList.remove("active");
+//     });
+//     contactLink.classList.add("active");
+//   });
+
+//   console.log("Executing on large screen");
+// }
+
+// Define a function to execute when the display size is below a certain width
+
 function executeOnLargeScreen() {
   // Your code for larger screens here
 
-  introLink.addEventListener("click", () => {
-    links.style.transform = "translateX(calc(50% - 75px))";
-    introLink.classList.add("active");
-    myWorkLinks.forEach((link) => {
-      link.classList.remove("active");
-    });
-    learnMoreLinks.forEach((link) => {
-      link.classList.remove("active");
-    });
-    contactLink.classList.remove("active");
-  });
-
-  myWorkLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      links.style.transform = "translateX(calc(25% - 75px))";
-      introLink.classList.remove("active");
-      myWorkLinks.forEach((link) => {
-        link.classList.add("active");
-      });
-      learnMoreLinks.forEach((link) => {
-        link.classList.remove("active");
-      });
-      contactLink.classList.remove("active");
-    });
-  });
-
-  learnMoreLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      links.style.transform = "translateX(calc(-25% + 75px))";
-      introLink.classList.remove("active");
-      myWorkLinks.forEach((link) => {
-        link.classList.remove("active");
-      });
-      learnMoreLinks.forEach((link) => {
-        link.classList.add("active");
-      });
-      contactLink.classList.remove("active");
-    });
-  });
-  contactLink.addEventListener("click", () => {
-    links.style.transform = "translateX(calc(-50% + 75px))";
+  // A common function to handle link clicks
+  function handleLinkClick(link, index) {
+    links.style.transform = `translateX(calc(${index}% - 75px))`;
+    // links.style.transform = `translateX(${index}%)`;
     introLink.classList.remove("active");
-    myWorkLinks.forEach((link) => {
-      link.classList.remove("active");
-    });
-    learnMoreLinks.forEach((link) => {
-      link.classList.remove("active");
-    });
-    contactLink.classList.add("active");
-  });
+    myWorkLinks.forEach((myWorkLink) => myWorkLink.classList.remove("active"));
+    learnMoreLinks.forEach((learnMoreLink) =>
+      learnMoreLink.classList.remove("active")
+    );
+    contactLink.classList.remove("active");
+    link.classList.add("active");
+  }
 
-  console.log("Executing on large screen");
+  // Event listeners for each link
+  introLink.addEventListener("click", () => handleLinkClick(introLink, 50));
+  myWorkLinks.forEach((link) => {
+    link.addEventListener("click", () => handleLinkClick(link, 25));
+  });
+  learnMoreLinks.forEach((link) => {
+    link.addEventListener("click", () => handleLinkClick(link, -25));
+  });
+  contactLink.addEventListener("click", () =>
+    handleLinkClick(contactLink, -50)
+  );
+
+  console.log("Executing on a large screen");
 }
 
-// Define a function to execute when the display size is below a certain width
+
 function executeOnSmallScreen() {
   // Your code for smaller screens here
 
   menuButton.addEventListener("click", () => {
-    // links.style.transform = "translateX(0)";
-    links.style.display = "flex";
+    links.style.height = "100vh";
+    // links.style.display = "flex";
     closeMenuBtn.style.display = "flex";
     menuButton.style.display = "none";
   });
   closeMenuBtn.addEventListener("click", () => {
-    // links.style.transform = "translateX(100vw)";
-    links.style.display = "none";
+    links.style.height = "0";
+    // links.style.display = "none";
     closeMenuBtn.style.display = "none";
     menuButton.style.display = "block";
   });
 
   menuLinks.forEach((link) => {
     link.addEventListener("click", () => {
-      // links.style.transform = "translateX(100vw)";
-      links.style.display = "none";
+      links.style.height = "0";
+      // links.style.display = "none";
       closeMenuBtn.style.display = "none";
       menuButton.style.display = "block";
       menuLinks.forEach((inactiveLink) => {
         inactiveLink.classList.remove("active");
       });
       link.classList.add("active");
+    });
+  });
+
+  myWorkLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menuLinks.forEach((inactiveLink) => {
+        inactiveLink.classList.remove("active");
+      });
+      myWorkLinks.forEach((workLink) => {
+        workLink.classList.add("active");
+      });
+    });
+  });
+
+  learnMoreLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menuLinks.forEach((inactiveLink) => {
+        inactiveLink.classList.remove("active");
+      });
+      learnMoreLinks.forEach((learnMoreLink) => {
+        learnMoreLink.classList.add("active");
+      });
     });
   });
 
